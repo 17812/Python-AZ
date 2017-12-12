528896d89d3197bb84d884dea0340b6efb29b88a
Tolga Dane <tdane@us.ibm.com>
2017-12-11 22:24:11 -0500

    first commit

---
+++ Tolga/workpad.py
@@ -0,0 +1,15 @@
+# def add_numbers(x, y, z=None, flag=False);
+#     if(flag):
+#         print 'flag is true'
+#    if(z=None):
+#        return x+y
+#    else:
+#        return x+y+z
+#
+#
+# print add_numbers(1,2,3, flag=true)
+
+
+x=(1, 2, 'a', 'b')
+
+type(x)
